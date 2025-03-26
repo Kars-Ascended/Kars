fa1a3a2b49eb2a5977d3c0fe589d81116a7c0a81
Josh White <whitejosh429@gmail.com>
2025-03-26 07:47:33 +0000

    base

---
+++ backend/reuse/nav.php
@@ -0,0 +1,5 @@
+<div class="nav">
+    <a href="index.php">Home</a>
+    <a href="about.php">About</a>
+    <a href="contact.php">Contact</a>
+</div>
---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/base.css">
+    <link rel="stylesheet" href="../css/nav.css">
+    <title>Home</title>
+    <?php include '../backend/reuse/nav.php'; ?>
+</head>
+
+<body class="main">
+    <p>test</p>
+</body>
+</html>
